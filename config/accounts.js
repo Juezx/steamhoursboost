e760706fb4b53d042010183402e1fdf394be24e8
Juezx <121260467+Juezx@users.noreply.github.com>
2023-01-24 05:09:34 +0700

    Add files via upload

---
+++ config/accounts.js
@@ -0,0 +1,18 @@
+var configsArray = [];
+var config;
+
+// 1st Account (you can boost many accounts as you want)
+config = {};
+config.username = 'all0veragain'; // Account username
+config.password = '59E2002y2015hx'; // Account password
+config.sharedSecret = 'BApBbzIfDvuZKmA2JEsAlx6VrLk'; // Shared secret (2FA only), leave it blank for steam guard code
+config.enableStatus = true; // Set it to false if you want to stay invisible
+config.gamesAndStatus = [
+	't.me/SteamHoursBoost', // Your custom status (counts as a game, you can only boost 31 games with the custom status)
+	730,
+	570,
+]; // IDs of the games, separated by comma
+
+
+
+module.exports = configsArray;
