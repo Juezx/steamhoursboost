44090c8d13c0de06475f9eb8557e33c46b12aea3
Juezx <121260467+Juezx@users.noreply.github.com>
2023-03-04 09:57:33 +0700

    Update accounts.js

--- config/accounts.js
+++ config/accounts.js
@@ -3,9 +3,9 @@ var config;
 
 // 1st Account (you can boost many accounts as you want)
 config = {};
-config.username = 'torif9'; // Account username
-config.password = 'Password98Ez'; // Account password
-config.sharedSecret = 'BApBbzIfDvuZKmA2JEsAlx6VrLk'; // Shared secret (2FA only), leave it blank for steam guard code
+config.username = ''; // Account username
+config.password = ''; // Account password
+config.sharedSecret = ''; // Shared secret (2FA only), leave it blank for steam guard code
 config.enableStatus = true; // Set it to false if you want to stay invisible
 config.gamesAndStatus = [
 	't.me/SteamHoursBoost', // Your custom status (counts as a game, you can only boost 31 games with the custom status)
